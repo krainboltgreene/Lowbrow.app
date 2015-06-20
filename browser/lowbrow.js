b9b34b946739df1486c5a8c7d6423688a4b7881a
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-06-19 20:26:37 -0500

    We renamed the default template

--- browser/lowbrow.js
+++ browser/lowbrow.js
@@ -16,7 +16,7 @@ Application.on("window-all-closed", function() {
 
 Application.on("ready", function() {
   main = new BrowserWindow({});
-  main.loadUrl("file://" + __dirname + "/../renderer/templates/application.html");
+  main.loadUrl("file://" + __dirname + "/../renderer/index.html");
   main.on("closed", function() {
     main = null;
   });
